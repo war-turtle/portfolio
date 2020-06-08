c5dd2ab772d97c3abebc54e7780ce715beebe6dc
war-turtle <kartikyadav71199@gmail.com>
2020-06-08 20:23:38 +0530

    basic animations added

--- site/src/index.js
+++ site/src/index.js
@@ -1,40 +1,70 @@
 import THREE from './dep/three';
-import { Turtle } from './mesh';
+import { Turtle, Cloud } from './mesh';
+import { randomInt } from './helper';
 
 const canvas = document.querySelector('#canvas');
 const renderer = new THREE.WebGLRenderer({ canvas, alpha: true, antialias: true });
+renderer.setClearColor(0x51c4f5);
 
-const fov = 75;
-const aspect = 2;
-const near = 0.1;
-const far = 1000;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+const near = -10;
+const far = 100;
+const width = 10;
+const height = 10;
+const camera = new THREE.OrthographicCamera(-width / 2, width / 2, height / 2, -height / 2, near, far);
+
+// const fov = 75;
+// const aspect = 2;
+// const near = 0.1;
+// const far = 1000;
+// const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
 var controls = new THREE.OrbitControls(camera, canvas);
-camera.position.set(0, 5, 0);
+camera.position.set(2, 2, 10);
 controls.update();
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x444444);
+scene.fog = new THREE.Fog(0x51c4f5, 40, 60);
 
 // Lights
-var lights = [];
-lights[0] = new THREE.PointLight(0xffffff, 1, 0);
-lights[1] = new THREE.PointLight(0xffffff, 2, 0);
-lights[2] = new THREE.PointLight(0xffffff, 2, 0);
 
-lights[0].position.set(0, 200, 100);
-lights[1].position.set(100, 200, 100);
-lights[2].position.set(- 100, - 200, - 100);
+const directional = new THREE.DirectionalLight(0xffffff, 1.5);
+directional.position.set(20, 20, 0);
 
-scene.add(lights[0]);
-scene.add(lights[1]);
-scene.add(lights[2]);
+scene.add(new THREE.AmbientLight(0xc5f5f5, 1));
+scene.add(directional);
 
-var axesHelper = new THREE.AxesHelper(3);
-scene.add(axesHelper);
+// var lights = [];
+// lights[0] = new THREE.PointLight(0xffffff, 1, 0);
+// lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+// lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+
+// lights[0].position.set(0, 200, 100);
+// lights[1].position.set(100, 200, 100);
+// lights[2].position.set(- 100, - 200, - 100);
+
+// scene.add(lights[0]);
+// scene.add(lights[1]);
+// scene.add(lights[2]);
+
+// var axesHelper = new THREE.AxesHelper(3);
+// scene.add(axesHelper);
 
 // Adding Meshes to the scene
 scene.add(new Turtle().mesh);
+scene.add(new Cloud(50, randomInt(-2, 2), randomInt(-2, 2)).mesh);
+scene.add(new Cloud(50, randomInt(-2, 2), randomInt(-2, 2)).mesh);
+scene.add(new Cloud(50, randomInt(-2, 2), randomInt(-2, 2)).mesh);
+scene.add(new Cloud(50, randomInt(-2, 2), randomInt(-2, 2)).mesh);
+
+(function createFloor() {
+
+    const floor = new THREE.Mesh(new THREE.PlaneBufferGeometry(100, 100), new THREE.MeshBasicMaterial({ color: 0xe0dacd }));
+    floor.rotation.x = -Math.PI / 2;
+    floor.position.y = -10;
+
+    scene.add(floor);
+}());
+
 
 function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -51,7 +81,14 @@ function resizeRendererToDisplaySize(renderer) {
 function render() {
     if (resizeRendererToDisplaySize(renderer)) {
         const canvas = renderer.domElement;
-        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+
+        camera.left = -canvas.clientWidth / 200;
+        camera.right = canvas.clientWidth / 200;
+        camera.top = canvas.clientHeight / 200;
+        camera.bottom = -canvas.clientHeight / 200;
+
+        // camera.aspect = canvas.clientWidth / canvas.clientHeight;
+
         camera.updateProjectionMatrix();
         controls.update();
     }
--- site/src/mesh/index.js
+++ site/src/mesh/index.js
@@ -1,14 +1,21 @@
 import THREE from "../dep/three";
+import gsap from 'gsap';
+import { randomInt } from "../helper";
 
 // Geometries
 const sphereGeometry = new THREE.SphereBufferGeometry(1, 6, 3, 0, Math.PI * 2, 0, 0.5 * Math.PI);
 const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
 
+// const wingGeometry = new THREE.BoxGeometry(1, 0.05, 1);
+// wingGeometry.vertices[0].x -= 0.5;
+// wingGeometry.vertices[2].x -= 0.5;
+
 // Materials
 const Materials = {
-    brown: new THREE.MeshPhongMaterial({ color: 'brown', flatShading: true }),
-    green: new THREE.MeshPhongMaterial({ color: 'green', flatShading: true }),
-    grey: new THREE.MeshPhongMaterial({ color: 'grey', flatShading: true })
+    brown: new THREE.MeshPhongMaterial({ color: '#442727', flatShading: true, shininess: 30 }),
+    green: new THREE.MeshPhongMaterial({ color: '#06623b', flatShading: true, shininess: 30 }),
+    grey: new THREE.MeshPhongMaterial({ color: 'grey', flatShading: true }),
+    clouds: new THREE.MeshPhongMaterial({ color: 0xeeeeee, flatShading: true }),
 };
 
 // Mesh classes
@@ -18,8 +25,11 @@ class Turtle {
 
         this.mesh.add(this.createShell());
         this.mesh.add(this.createFace());
-        this.mesh.add(this.createEye());
         this.mesh.add(this.createLeg());
+
+        this.mesh.position.set(-1, 0, 0);
+
+        this.animateTurtle();
     }
 
     createShell() {
@@ -30,38 +40,159 @@ class Turtle {
     }
 
     createFace() {
+        const group = new THREE.Group;
+
         const face = new THREE.Mesh(boxGeometry, Materials.green);
         face.position.set(1, 0.2, 0);
         face.scale.set(0.5, 0.4, 0.4);
 
-        return face;
-    }
-
-    createEye() {
         const eye = new THREE.Mesh(boxGeometry, Materials.grey);
         eye.position.set(1.05, 0.2, 0.2);
-        eye.scale.set(0.05, 0.15, 0.05);
+        eye.scale.set(0.05, 0.01, 0.05);
 
-        return eye;
+        group.add(face);
+        group.add(eye);
+
+        this.animateFace(face);
+        this.animateEye(eye);
+
+        return group;
     }
 
     createLeg() {
         const legs = new THREE.Group();
 
+        const frontLegPivot = new THREE.Group();
+        frontLegPivot.position.set(0.5, 0, 0.7);
+        frontLegPivot.rotation.set(0, Math.PI / 6, 0);
+
         const frontLeg = new THREE.Mesh(boxGeometry, Materials.green);
-        frontLeg.position.set(0.5, 0.1, 0.8);
+        frontLeg.position.set(0, 0.1, 0.05);
         frontLeg.scale.set(0.2, 0.2, 0.3);
-        frontLeg.rotation.set(0, Math.PI / 6, 0);
+
+        frontLegPivot.add(frontLeg);
+
+        const backLegPivot = new THREE.Group();
+        backLegPivot.position.set(-0.5, 0.1, 0.5);
+        backLegPivot.rotation.set(0, -Math.PI / 6, 0);
 
         const backLeg = new THREE.Mesh(boxGeometry, Materials.green);
-        backLeg.position.set(-0.6, 0.1, 0.75);
+        // backLeg.position.set(-0.6, 0.1, 0.75);
+        backLeg.position.set(-0.1, 0, 0.25);
         backLeg.scale.set(0.2, 0.2, 0.25);
-        backLeg.rotation.set(0, -Math.PI / 6, 0);
 
-        legs.add(frontLeg);
-        legs.add(backLeg);
+        backLegPivot.add(backLeg);
+
+        const point = new THREE.Geometry();
+        point.vertices.push(
+            new THREE.Vector3(-0.5, 0.1, 0.5)
+        );
+
+        legs.add(new THREE.Points(point, new THREE.PointsMaterial({ color: "red", size: 0.1 })))
+
+        this.animateLegs(frontLegPivot, backLegPivot);
+
+        legs.add(frontLegPivot);
+        legs.add(backLegPivot);
         return legs;
+    }
+
+    animateTurtle() {
+        gsap.to(this.mesh.position, {
+            duration: 1,
+            y: 0.2,
+            yoyo: true,
+            repeat: -1,
+            ease: "sine.inOut",
+        });
+    }
+
+    animateLegs(frontLegPivot, backLegPivot) {
+        gsap.to(frontLegPivot.rotation, {
+            duration: 1,
+            y: 0,
+            x: Math.PI / 6,
+            repeat: -1,
+            yoyo: true,
+            ease: "power1.inOut"
+        });
+
+        gsap.to(backLegPivot.rotation, {
+            duration: 1,
+            y: -Math.PI / 3,
+            x: Math.PI / 6,
+            repeat: -1,
+            yoyo: true,
+            ease: "power1.inOut"
+        })
+    }
+
+    animateFace(face) {
+        gsap.to(face.rotation, {
+            duration: 1,
+            z: -Math.PI / 10,
+            yoyo: true,
+            repeat: -1,
+            ease: "sine.inOut",
+        });
+    }
+
+    animateEye(eye) {
+        gsap.to(eye.scale, {
+            duration: 1,
+            y: 0.1,
+            repeat: -1,
+            yoyo: true,
+        });
     }
 }
 
-export { Turtle };
+class Cloud {
+    constructor(x, y, z) {
+        this.mesh = this.createClouds(x, y, z);
+
+        this.animateCloud();
+    }
+
+    createClouds(x, y, z) {
+        const clouds = new THREE.Group();
+
+        const cloudGeo = new THREE.SphereGeometry(5, 4, 6);
+        const cloud1 = new THREE.Mesh(cloudGeo, Materials.clouds);
+        cloud1.scale.set(1, 0.8, 1);
+
+        const cloud2 = cloud1.clone();
+        cloud2.scale.set(.55, .35, 1);
+        cloud2.position.set(5, -1.5, 2);
+
+        const cloud3 = cloud1.clone();
+        cloud3.scale.set(.75, .5, 1);
+        cloud3.position.set(-5.5, -2, -1);
+
+        clouds.add(cloud1);
+        clouds.add(cloud2);
+        clouds.add(cloud3);
+
+        clouds.scale.set(0.1, 0.1, 0.1);
+        clouds.position.set(x, y, z);
+
+        return clouds;
+    }
+
+    animateCloud() {
+        gsap.to(this.mesh.position, {
+            duration: 5,
+            x: -20,
+            repeat: -1,
+            repeatDelay: 0,
+            delay: randomInt(0, 10),
+            repeatDelay: randomInt(0, 5),
+            onRepeat: () => {
+                this.mesh.position.y = randomInt(-2, 2);
+                this.mesh.position.z = randomInt(-2, 2);
+            }
+        });
+    }
+}
+
+export { Turtle, Cloud };
